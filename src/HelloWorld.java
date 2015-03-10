69be496e9a9ab599a23bfcd21b645eebaf6f3fd3
dzejbi <janek.bialkowski@gmail.com>
2015-03-10 12:41:44 +0100

    dsa

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello worlddddd!");
+		System.out.println("Hello worldddddasd!");
 		// TODO Auto-generated method stub
 
 	}
